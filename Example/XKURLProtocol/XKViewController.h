673986bbbe0165ae55aacf47d2cd3c5ab99eeb7a
Chester <>
2020-07-22 20:32:09 +0800

    Initial commit

---
+++ Example/XKURLProtocol/XKViewController.h
@@ -0,0 +1,13 @@
+//
+//  XKViewController.h
+//  XKURLProtocol
+//
+//  Created by xukaiv5 on 07/22/2020.
+//  Copyright (c) 2020 xukaiv5. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XKViewController : UIViewController
+
+@end
